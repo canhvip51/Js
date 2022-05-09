8c14c63197040328e022fd04ce88191253e3d295
canhlt <canhvip51@gmail.com>
2022-05-10 00:17:23 +0700

    update

--- workArray2.js
+++ workArray2.js
@@ -1,37 +1,80 @@
-var arrs=[
-    {
-        id:1,
-        name:'js',
-        coin:40
+var arrs = [{
+        id: 1,
+        name: 'js',
+        coin: 40
     },
     {
-        id:2,
-        name:'java',
-        coin:0
+        id: 2,
+        name: 'java',
+        coin: 0
     },
     {
-        id:3,
-        name:'html',
-        coin:20
+        id: 3,
+        name: 'html',
+        coin: 20
     },
     {
-        id:1,
-        name:'css',
-        coin:10
+        id: 4,
+        name: 'css',
+        coin: 10
     },
-    ]
+    {
+        id: 5,
+        name: 'css',
+        coin: 10
+    },
+]
 
 // forEach() lặp  qua các phần tử
-arrs.forEach(function(arr,index){
-// console.log(index,arr);
+arrs.forEach(function(arr, index) {
+    // console.log(index,arr);
 });
 // every() các phần tử phải thỏa mãn 1 đk nào đó
-var isFree= arrs.every(function(arr,index){
-    return arr.coin===0;
+var isFree = arrs.every(function(arr, index) {
+    return arr.coin === 0;
 });
-console.log(isFree)
+// console.log(isFree)
 // some() chỉ cần phần tử phải thỏa mãn 1 đk nào đó
+var isFree = arrs.some(function(arr, index) {
+    return arr.coin === 0;
+});
+// console.log(isFree)
 // find() trả về phần tử đầu tiên thỏa mãn
+var item = arrs.find(function(arr, indec) {
+    return arr.name == 'css';
+});
+// console.log(item)
 // filter() trả về array toàn bộ phần tử thỏa mãn
-// map()
-// reduce()
+var items = arrs.filter(function(arr, indec) {
+    return arr.name == 'css';
+});
+// console.log(items)
+//map() sửa các phần tử của array
+//Thực hiện vòng lặp lặp qua tất cả phần tử 
+function handler(arr, index) {
+    return {
+        id: arr.id,
+        name: `Khoa hoc: ${arr.name}`,
+        coint: arr.coin,
+        coinText: `Gia khoa hoc = ${arr.coin}`
+    };
+};
+var newArr = arrs.map(handler);
+// console.log(newArr)
+// console.log(arrs)
+// reduce() nhận 1 giá trị duy nhất sau khi tính toán
+var i = 0;
+
+function coinHandler(accumlator, currentValue, currentIndex, originArray) {
+    var tong = accumlator + currentValue.coin;
+    console.table({
+        'Lượt chạy': ++i,
+        'Biến tích trữ': accumlator,
+        'Giá khóa học': currentValue.coin,
+        'Tích trữ đc': tong
+    });
+    console.log(currentValue);
+    return tong;
+}
+var totalCoin = arrs.reduce(coinHandler, 0);
+console.log(totalCoin)
