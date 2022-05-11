2579cb67e7ab1b23c9bbd14049b26a94342937d5
canhlt <canhvip51@gmail.com>
2022-05-12 00:31:16 +0700

    update 12/5/2022

--- callback.js
+++ callback.js
@@ -28,4 +28,96 @@ Array.prototype.map2 = function(callback) {
 var html = khoahocs.map2(function(khoahoc, index) {
     return `<h2>${khoahoc}</h2>`;
 });
-console.log(html.join(''))
+// console.log(html.join(''))
+//find
+var arrs = [{
+        id: 1,
+        name: 'js',
+        coin: 40
+    },
+    {
+        id: 2,
+        name: 'java',
+        coin: 20
+    },
+    {
+        id: 3,
+        name: 'html',
+        coin: 20
+    },
+    {
+        id: 4,
+        name: 'java',
+        coin: 0
+    },
+]
+arrs.length = 100;
+arrs.push({
+    id: 5,
+    name: 'css',
+    coin: 0
+})
+Array.prototype.find2 = function(callback) {
+        if (typeof callback == 'function') {
+            for (let i = 0; i < this.length; ++i)
+                if (callback(this[i], i, this)) {
+                    return this[i];
+                }
+        }
+    }
+    // var isFind = arrs.find2(function(item, index) {
+    //         return item.name == 'java';
+    //     })
+    // console.log(isFind)
+Array.prototype.filter2 = function(callback) {
+    if (typeof callback == 'function') {
+        var arr = [];
+        for (var i in this) {
+            console.log(i)
+            if (!this.hasOwnProperty(i)) {
+                break;
+            }
+            if (callback(this[i], i, this)) {
+                arr.push(this[i]);
+            }
+        }
+        return arr;
+    }
+}
+var isFilter = arrs.filter2(function(item, index) {
+    return item.name == 'java';
+})
+console.log(isFilter)
+Array.prototype.every2 = function(callback) {
+    if (typeof callback == 'function') {
+        for (var i in this) {
+            console.log(i)
+            if (!this.hasOwnProperty(i)) {
+                break;
+            }
+            if (!callback(this[i], i, this)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+var isEvery = arrs.every2(function(item, index) {
+        return item.coin == 0;
+    })
+    // console.log(isEvery)
+
+Array.prototype.some2 = function(callback) {
+    if (typeof callback == 'function') {
+        for (let i = 0; i < this.length; ++i) {
+            if (callback(this[i], i, this)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+var isEvery = arrs.some2(function(item, index) {
+        return item.coin == 0;
+    })
+    // console.log(isEvery)
